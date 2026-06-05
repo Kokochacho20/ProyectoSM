2630203467961bf499e648c611ed7e909de2e2a5
marcela.pozo <marcel14navarro@gmail.com>
2026-06-04 19:39:18 -0600

    Add project files.

---
+++ PA_WEB/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PA_WEB.Models;
+
+namespace PA_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IniciarSesion(InicioSesionModel model)
+        {
+            // implementar logica de inicio
+            return RedirectToAction("Inicio", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
---
+++ PA_WEB/Models/InicioSesionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PA_WEB.Models
+{
+    public class InicioSesionModel
+    {
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Contrasenna { get; set; } = string.Empty;
+    }
+}
